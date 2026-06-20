abb424e7c788692ad09930f50b2ec551ea8033a6
gotursneha <snehagotur99@gmail.com>
2026-06-20 22:30:16 +0530

    initial commit

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -5,7 +5,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertTrue;
-public class WebPageTest
+public class main
 {
     private static WebDriver driver;
     @BeforeTest
